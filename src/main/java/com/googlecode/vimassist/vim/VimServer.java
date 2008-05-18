a02452752bfaa841fa79b2490e29943178675693
virasak <virasak@49eca342-4535-0410-bcc3-1970b16363d6>
2008-05-18 22:12:49 +0000

    fixed too fast sending move window position command to vim

--- src/main/java/com/googlecode/vimassist/vim/VimServer.java
+++ src/main/java/com/googlecode/vimassist/vim/VimServer.java
@@ -28,40 +28,46 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class VimServer {
+public class VimServer implements Runnable {
 	private static final String SERVER_NAME = "--servername";
 	private static final String REMOTE_TAB_SILENT = "--remote-tab-silent";
 	private static final String REMOTE_SEND = "--remote-send";
 	private String vimPath;
 	private String serverName;
 	private File directory;
+	private Queue<List<String>> commandQueue = new ConcurrentLinkedQueue<List<String>>();
 
 	public VimServer() {
 		this(new File("."));
 	}
-	
+
 	public VimServer(File directory) {
 		this("SERVER", directory);
 	}
-	
+
 	public VimServer(String serverName, File directory) {
 		this("gvim", serverName, directory);
 	}
-	
+
 	public VimServer(String vimPath, String serverName, File directory) {
 		this.vimPath = vimPath;
 		this.serverName = serverName + "@" + System.currentTimeMillis();
 		this.directory = directory;
+
+		new Thread(this).start();
 	}
-	
+
 	public void open() {
 		try {
 			sendCommand();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public void openFile(File file) {
@@ -71,9 +77,9 @@ public class VimServer {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}	
+		}
 	}
-	
+
 	public void quit() {
 		try {
 			sendCommand(REMOTE_SEND, ":q!");
@@ -81,19 +87,57 @@ public class VimServer {
 			e.printStackTrace();
 		}
 	}
-	
-	public Process sendCommand(String ...args) throws IOException {
+
+	public void sendCommand(String ...args) throws IOException {
 		ArrayList<String> commandList = new ArrayList<String>();
 		Collections.addAll(commandList, vimPath, SERVER_NAME, serverName);
 		Collections.addAll(commandList, args);
-		return new ProcessBuilder(commandList).directory(directory).start();	
+		commandQueue.add(commandList);
 	}
-	
-	public Process sendRemoteCommand(String ...args) throws IOException {
+
+	public void sendRemoteCommand(String ...args) throws IOException {
 		ArrayList<String> commandList = new ArrayList<String>();
 		Collections.addAll(commandList, vimPath, SERVER_NAME, serverName, REMOTE_SEND);
 		Collections.addAll(commandList, args);
-		return new ProcessBuilder(commandList).directory(directory).start();	
+		commandQueue.add(commandList);
 	}
-		
+
+	@Override
+	public void run() {
+		while (true) {
+			List<String> commandList = commandQueue.poll();
+			if ( commandList != null) {
+
+				// move vim window too fast may get system error
+				// so get only the last test move command in sequence
+				if (commandList.get(commandList.size() - 1).startsWith(":winpos")) {
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					List<String> headCommandList = commandQueue.peek();
+					while (headCommandList != null && headCommandList.get(headCommandList.size() - 1).startsWith(":winpos")) {
+						commandList = commandQueue.poll();
+						headCommandList = commandQueue.peek();
+					}
+				}
+				try {
+					new ProcessBuilder(commandList).directory(directory).start();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// do nothing
+				}
+			}
+		}
+	}
+
+
 }
